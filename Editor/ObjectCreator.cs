f91b05ccfc0597c105c460f8934641716ce3f6d1
Tymon Oziemblewski <tymon.oziemblewski@gmail.com>
2023-04-09 20:57:56 +0200

    Execute more menu items: PhysicMaterials, Animations, Materials

--- Editor/ObjectCreator.cs
+++ Editor/ObjectCreator.cs
@@ -1,6 +1,6 @@
 using UnityEditor;
 
-public class ObjectCreator
+public static class ObjectCreator
 {
     [MenuItem("Assets/Create Object %n")]
     public static void CreateObject()
@@ -9,6 +9,10 @@ public class ObjectCreator
         if (path.Contains("Scripts")) EditorApplication.ExecuteMenuItem("Assets/Create/C# Script");
         else if (path.Contains("Scenes")) EditorApplication.ExecuteMenuItem("Assets/Create/Scene");
         else if (path.Contains("Prefabs")) EditorApplication.ExecuteMenuItem("Assets/Create/Prefab");
+        else if (path.Contains("PhysicMaterials")) EditorApplication.ExecuteMenuItem("Assets/Create/Physic Material");
+        else if (path.Contains("Physic Materials")) EditorApplication.ExecuteMenuItem("Assets/Create/Physic Material");
+        else if (path.Contains("Materials")) EditorApplication.ExecuteMenuItem("Assets/Create/Material");
+        else if (path.Contains("Animations")) EditorApplication.ExecuteMenuItem("Assets/Create/Animation");
 
         if (!string.IsNullOrEmpty(path)) return;
 
