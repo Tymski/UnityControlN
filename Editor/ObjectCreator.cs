6457eb5e574bd56e9ae5b43cbe4a27080f4d260b
Tymon Oziemblewski <tymon.oziemblewski@gmail.com>
2025-01-23 21:23:03 +0100

    Unity 6 support

--- Editor/ObjectCreator.cs
+++ Editor/ObjectCreator.cs
@@ -2,6 +2,7 @@ using UnityEditor;
 
 public static class ObjectCreator
 {
+#if !UNITY_6000_0_OR_NEWER
     [MenuItem("Assets/Create Object %n")]
     public static void CreateObject()
     {
@@ -19,4 +20,28 @@ public static class ObjectCreator
         if (Selection.gameObjects.Length > 1) EditorApplication.ExecuteMenuItem("GameObject/Create Empty Parent");
         else EditorApplication.ExecuteMenuItem("GameObject/Create Empty Child");
     }
+#endif
+
+#if UNITY_6000_0_OR_NEWER
+    [MenuItem("Assets/Create Object %n")]
+    public static void CreateObjectForUnity6()
+    {
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+        if (path.Contains("Scripts")) EditorApplication.ExecuteMenuItem("Assets/Create/Scripting/MonoBehaviour Script");
+        else if (path.Contains("Scenes")) EditorApplication.ExecuteMenuItem("Assets/Create/Scene/Scene");
+        else if (path.Contains("Prefabs")) EditorApplication.ExecuteMenuItem("Assets/Create/Scene/Prefab");
+        else if (path.Contains("PhysicMaterials")) EditorApplication.ExecuteMenuItem("Assets/Create/Physics Material");
+        else if (path.Contains("Physic Materials")) EditorApplication.ExecuteMenuItem("Assets/Create/Physics Material");
+        else if (path.Contains("PhysicsMaterials")) EditorApplication.ExecuteMenuItem("Assets/Create/Physics Material");
+        else if (path.Contains("Physics Materials")) EditorApplication.ExecuteMenuItem("Assets/Create/Physics Material");
+        else if (path.Contains("Materials")) EditorApplication.ExecuteMenuItem("Assets/Create/Rendering/Material");
+        else if (path.Contains("Animations")) EditorApplication.ExecuteMenuItem("Assets/Create/Animation/Animation Clip");
+
+        if (!string.IsNullOrEmpty(path)) return;
+
+        if (Selection.gameObjects.Length > 1) EditorApplication.ExecuteMenuItem("GameObject/Create Empty Parent");
+        else EditorApplication.ExecuteMenuItem("GameObject/Create Empty Child");
+    }
+#endif
 }
